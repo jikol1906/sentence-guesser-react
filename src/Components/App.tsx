ac7447c7d65cfc6720e66f8b202ee8b12f0ee51c
Boris Grunwald <borisgrunwald@gmail.com>
2022-10-12 13:55:07 +0200

    remove border radius input

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -260,7 +260,7 @@ const App: React.FunctionComponent<ITest2Props> = (props) => {
         
         {enteringSentence ?        
           <form id="sentenceform" onSubmit={translate} className="space-y-8 grid">
-            <input required placeholder="Enter sentence to translate..." ref={sentenceInputRef} className="p-2 w-full outline-none bg-transparent m-auto border-b-2 " type="text" name="" id="" />
+            <input required placeholder="Enter sentence to translate..." ref={sentenceInputRef} className="p-2 w-full outline-none bg-transparent m-auto border-b-2 rounded-none" type="text" name="" id="" />
             <div className="justify-self-center">
             <Button  type="submit" >Translate sentence</Button>
             </div>
