371dfb9cd48185f9b950add2fcb621d7323c83fe
Boris Grunwald <borisgrunwald@gmail.com>
2022-11-01 22:24:54 +0100

    added data attributes

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -11,8 +11,10 @@ ReactModal.setAppElement("#root")
 const Modal: React.FunctionComponent<IModalProps> = () => {
     const {closeModal, modalContent,modal} = React.useContext(ModalCtx);
 
-  return <ReactModal shouldCloseOnOverlayClick onRequestClose={() => closeModal()} overlayClassName="fixed inset-0 bg-black/75 flex items-center justify-center px-4 sm:px-7" className="bg-slate-800 outline-none text-white p-5 md:p-8 text-center max-w-5xl rounded-md" isOpen={modal}>
-    {modalContent}
+  return <ReactModal data-test="modal" shouldCloseOnOverlayClick onRequestClose={() => closeModal()} overlayClassName="fixed inset-0 bg-black/75 flex items-center justify-center px-4 sm:px-7" className="bg-slate-800 outline-none text-white p-5 md:p-8 text-center max-w-5xl rounded-md" isOpen={modal}>
+    <div className="">
+      {modalContent}
+    </div>
     <Button onClick={() => closeModal()}>Close</Button>
   </ReactModal>;
 };
--- src/Components/Word.tsx
+++ src/Components/Word.tsx
@@ -48,11 +48,12 @@ const Word: React.FunctionComponent<IWordProps> = ({
         { "--animation-delay": `${wordNum * 0.02}s` } as React.CSSProperties
       }
     >
-      <div className="space-x-1 text-base sm:text-2xl md:text-3xl">
+      <div data-test-letterinputcontainer={wordNum} className="space-x-1 text-base sm:text-2xl md:text-3xl">
         {word.map((s, i) => (
           <LetterInput
             key={i} //Should be ok to use indexes as keys since order of inputs doesn't change
             autoFocus={wordNum === 0 && i === 0}
+            data-letter-input={`${wordNum}${i}`}
             isNonCharacter={!isCharacter(s, languageToTranslateInto)}
             onInput={(e) => onLetterInput(e, wordNum, i)}
             onKeyUp={(e) => onLetterKeyUp(e, wordNum, i)}
@@ -64,6 +65,7 @@ const Word: React.FunctionComponent<IWordProps> = ({
       </div>
       <button
         onMouseDown={(e) => e.preventDefault()}
+        data-test-reavealbutton={wordNum}
         onClick={revealRandLetter}
         className="mt-4 text-xs opacity-40 hover:opacity-100"
       >
