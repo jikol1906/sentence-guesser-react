96eff7796614d7bda1ae4e685aa28e3dd54fedd5
Boris Grunwald <borisgrunwald@gmail.com>
2025-06-26 14:01:03 +0200

    refactor: simplify Paragraph component by removing convertSentenceToGuess function and directly mapping words

--- src/Components/Paragraph.tsx
+++ src/Components/Paragraph.tsx
@@ -9,48 +9,22 @@ interface ISentenceGuesserProps {
   paragraphIndex: number;
 }
 
-export type WordWithIndexes = {
-  word: string[];
-  indexes: number[];
-};
-
-/**
- * Converts a sentence into an array of words, where each word is represented as an object
- * containing its characters split into an array and an array of indexes. The indexes go from 0-n where n is the total length of the sentence
- * @param sentence The sentence to convert
- * @return An array of objects, each containing a word and its indexes
- * */
-const convertSentenceToGuess = (sentence: string): WordWithIndexes[] => {
-  let currIndex = 0;
-  return sentence.split(" ").map((word) => {
-    const wordData: WordWithIndexes = {
-      word: [...word],
-      indexes: Array.from({ length: word.length }, (_, i) => currIndex + i),
-    };
-    currIndex += word.length;
-    return wordData;
-  });
-};
-
 const Paragraph: React.FunctionComponent<ISentenceGuesserProps> = ({
   languageToTranslateInto,
   paragraph,
   paragraphIndex,
 }) => {
-  const wordsWithIndexes = convertSentenceToGuess(paragraph.sentenceToGuess);
-  const lastLetterIndex =
-    paragraph.sentenceToGuess.replace(/ /g, "").length - 1;
 
   return (
     <div className="space-y-8">
       <div className="sticky text-lg top-7 p-4 bg-slate-600 z-10 rounded-md">
         <p>{paragraph.sentenceToShow}</p>
       </div>
-      {wordsWithIndexes.map((wordData, i) => (
+      {paragraph.sentenceToGuess.split(" ").map((word, i) => (
         <Word
           key={i}
           paragraphIndex={paragraphIndex}
-          wordData={wordData}
+          word={word}
           languageToTranslateInto={languageToTranslateInto}
           animationDelay={`${i * 0.02}s`}
           
