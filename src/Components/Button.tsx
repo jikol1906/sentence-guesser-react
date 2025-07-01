963bb3fcac87ef8994beb8c46b2744e9605c1bb3
Boris Grunwald <borisgrunwald@gmail.com>
2025-07-01 11:18:19 +0200

    feat: enhance Button component with buttonType prop for customizable styles

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,19 +1,33 @@
 import * as React from "react";
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    active?: boolean
+    active?: boolean;
+    buttonType?: 'danger' | 'primary';
 }
 
+const ButtonTypeClasses: Record<NonNullable<IButtonProps['buttonType']>, string> = {
+    primary: 'bg-blue-600 hover:bg-blue-500 text-white',
+    danger: 'bg-red-500 hover:bg-red-600 text-white',
+};
+
+const ButtonTypeActiveClasses: Record<NonNullable<IButtonProps['buttonType']>, string> = {
+    primary: 'bg-blue-500',
+    danger: 'bg-red-600',
+};
+
 const Button: React.FunctionComponent<IButtonProps> = (props) => {
-    const {children, className, active,...rest} = props
-  return (
-    <button
-      className={`px-4 py-2 rounded-lg  ${className} ${active ? "bg-slate-500" : "bg-slate-600 hover:bg-slate-500"}`}
-      {...rest}
-    >
-        {children}
-    </button>
-  );
+    const { children, className, active, buttonType = 'primary', ...rest } = props;
+
+    return (
+        <button
+            className={`px-4 py-2 rounded-lg ${className} ${
+                active ? ButtonTypeActiveClasses[buttonType] : ButtonTypeClasses[buttonType]
+            }`}
+            {...rest}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
